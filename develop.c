f000246a1baaa941420055bf60bc11c3bb2f04c6
yulindong <yljzxp@sina.com>
2019-07-16 04:22:25 -0400

    develop

---
+++ develop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+	printf("-----------------");
+	printf("-----------------");
+	printf("-----------------");
+	printf("-----------------");
+	printf("-----------------");
+	printf("-----------------");
+	printf("-----------------");
+	return 0;
+}
